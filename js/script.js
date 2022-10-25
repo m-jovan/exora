af96dfa439c7b3bba394b7a3ef7aeaa0a9d90b8f
Jovan Milosevic <jovan.milosevic810@gmail.com>
2022-10-25 20:16:11 +0200

    Improve SEO

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 document.documentElement.dataset.scroll = window.scrollY;
 
-document.addEventListener("scroll", () => {
-  document.documentElement.dataset.scroll = window.scrollY;
-});
+document.addEventListener(
+  "scroll",
+  () => {
+    document.documentElement.dataset.scroll = window.scrollY;
+  },
+  { passive: true }
+);
+
+document.addEventListener("touchstart", () => {}, { passive: true });
